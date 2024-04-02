8b7908c7bede42f827a54c42948876b20cd0b347
Alex Apostolakis <alex.apostolakis@noa.gr>
2024-04-02 14:03:42 +0300

    organize 6

--- ML_classic/run_fires_test_sk.sh
+++
@@ -1,4 +0,0 @@
-cp /mnt/nvme2tb/ffp/code/mlfires/ML_fires_al/space_new_test_sk.py /mnt/nvme2tb/ffp/code/mlfires/ML_fires_al/space_new_test.py
-python -u /mnt/nvme2tb/ffp/code/mlfires/ML_fires_al/fires_test.py
-
-
